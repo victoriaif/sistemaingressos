083d6cc9f3ba0e0f33d9ab4766cb624c0b7eb4dd
Maria <dismice@gmail.com>
2025-07-07 21:21:29 -0300

    Cria try catch no TransacaoController e exibe mensagens de Erro, incluídas no  listar.html do Ingresso

--- sistemaingressos/src/main/java/br/com/sistemaingressos/controller/TransacaoController.java
+++ sistemaingressos/src/main/java/br/com/sistemaingressos/controller/TransacaoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/transacoes")
@@ -68,10 +69,15 @@ public class TransacaoController {
         return "redirect:/transacoes";
     }
 
-    // COMPRAR ingresso e SALVAR transacao
     @PostMapping("/comprar/{idIngresso}")
-    public String comprar(@PathVariable Long idIngresso) {
-        transacaoService.comprarIngresso(idIngresso);
+    public String comprar(@PathVariable Long idIngresso, RedirectAttributes redirectAttributes) {
+        try {
+            transacaoService.comprarIngresso(idIngresso);
+            redirectAttributes.addFlashAttribute("sucesso", "Compra realizada com sucesso!");
+        } catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute("erro", e.getMessage());
+        }
+
         return "redirect:/ingressos";
     }
 }
